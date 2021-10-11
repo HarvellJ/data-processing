cdfadb8c8b2e9ab619e749a44cb1da175b436ab7
Joshua Harvey <jharveycreates@gmail.com>
2021-10-11 10:37:15 +0100

    Moved source so it will be clearer when adding infra files.

---
+++ src/OrderProcessor/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrderProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        private IOrderLogic orderLogic;
+
+        public OrderController(ILogger<OrderController> logger, IOrderLogic orderLogic)
+        {
+            _logger = logger;
+            this.orderLogic = orderLogic;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return "it works.";
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<string> PostAsync(Order orderContent)
+        {
+            await orderLogic.WriteOrder(orderContent);
+            return string.Format("Received order: \n {0}", orderContent.OrderContent);
+        }
+    }
+}
